096f876b398b3df383a8b72104a794b774daed43
Yu Li <liyu3519@gmail.com>
2024-09-26 08:15:33 +0100

    feat: finish a new challenge bank-account; finish react-quiz project

---
+++ 10-react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function FinishScreen({
+  points,
+  maxPossiblePoints,
+  highScore,
+  dispatch,
+}) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji = "🎉";
+  if (percentage === 100) emoji = "🏅";
+
+  return (
+    <>
+      {" "}
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} {Math.ceil(percentage)}%
+      </p>
+      <p className="highscore">Highscore: {highScore} points</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => {
+          dispatch({ type: "restart" });
+        }}
+      >
+        Restart?
+      </button>
+    </>
+  );
+}
